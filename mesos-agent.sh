0a8ee22e051c82a7e67608e42a3c4eab4ed1ddb5
Yves Thrier <yves.thrier@zuehlke.com>
2016-06-07 13:57:02 +0200

    add sudo to mesos agent, otherwise no processes can be created

--- mesos-agent.sh
+++ mesos-agent.sh
@@ -5,4 +5,4 @@
 
 hostIp=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
 
-$HOME_MESOS/build/bin/mesos-slave.sh --ip=$hostIp --work_dir=/var/lib/mesos --log_dir=/var/lib/mesos/log --master=$MASTER_MESOS
+sudo $HOME_MESOS/build/bin/mesos-slave.sh --ip=$hostIp --work_dir=/var/lib/mesos --log_dir=/var/lib/mesos/log --master=$MASTER_MESOS
